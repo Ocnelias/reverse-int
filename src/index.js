099ff3fc5a9090dc2758afa527e5c5f546d39f67
ocnelias <ocnelias@gmail.com>
2021-03-11 23:28:46 +0200

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let res = [];
+    n = Math.abs(n);
+    n = n.toString().split('');
+    for (let i = n.length; i >= 0; i--) {
+        res.push(n[i]);
+    }
+    return res.join('');
 }
